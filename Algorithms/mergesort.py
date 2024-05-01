cc1644aceac2b2c7388429491278906a49ad4477
Michael Scoleri <mikesyanks02@icloud.com>
2024-04-30 20:48:25 -0400

    ORganized

--- Algorithms/mergesort.py
+++ Algorithms/mergesort.py
@@ -32,7 +32,7 @@ def merge_sort(array):
         return array
     mid = len(array) // 2
     left = merge_sort(array[:mid])
-    right = merge_sort(array[mid:])
+    right = merge_sort
 
     return merge(left, right)
 
